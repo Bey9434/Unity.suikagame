9f28804b22df7be50a021ce03eb5186fc454d8fd
Bey9434 <aluca9434@gmail.com>
2023-12-17 22:32:22 +0900

    画面の位置調整

--- Assets/scripts/sphere.cs
+++ Assets/scripts/sphere.cs
@@ -20,32 +20,48 @@ public class sphere : MonoBehaviour
 
     [SerializeField]
    public int _Spheresize;
+
    
 
   void Update()
    {
-        
-         _mousePosition = Input.mousePosition;
-         _mousePosition.x = Mathf.Clamp(_mousePosition.x,0,Camera.main.pixelWidth);
-         _mousePosition.y = 800f;
-         _mousePosition.z = 100f;
+      _mousePosition = Input.mousePosition;
+      _mousePosition.z = 100f; // カメラから球までの距離。プレハブの距離がキャンバス基準のrecttransformなので、その分の補正が必要。
+      _mousePosition.y = 750f;
+      //Debug.Log(_mousePosition);
 
+    // スクリーン座標からワールド座標への変換
+      Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+      Debug.Log(worldMousePosition);
 
       if(Input.GetMouseButtonDown(0))
       {
-         index = Random.Range(0,5);
-         _previewSphere = Instantiate(_seeds[index], Camera.main.ScreenToWorldPoint(_mousePosition),Quaternion.identity);
+         index = Random.Range(0, 5); 
+         _previewSphere = Instantiate(_seeds[index], AdjustPosition(worldMousePosition, _seeds[index].transform.localScale.x / 2), Quaternion.identity);
       }
+
       if(Input.GetMouseButton(0) && _previewSphere != null)
       {
-         _previewSphere.transform.position = Camera.main.ScreenToWorldPoint(_mousePosition);
+         _previewSphere.transform.position = AdjustPosition(worldMousePosition, _previewSphere.transform.localScale.x / 2);
       }
-      if(Input.GetMouseButtonUp(0))
+
+      if(Input.GetMouseButtonUp(0) && _previewSphere != null)
       {
          Destroy(_previewSphere);
-        Instantiate(_seeds[index], Camera.main.ScreenToWorldPoint(_mousePosition),Quaternion.identity);
+         Instantiate(_seeds[index], AdjustPosition(worldMousePosition, _seeds[index].transform.localScale.x / 2), Quaternion.identity);
       }
-      
    }
 
+// 生成位置を調整するメソッド
+   Vector3 AdjustPosition(Vector3 position, float radius)
+   {
+      float leftWall = -5.25f + radius;
+      float rightWall = 5.25f - radius;
+      position.x = Mathf.Clamp(position.x, leftWall, rightWall);
+      return position;
+   }
+
+      
 }
+
+
